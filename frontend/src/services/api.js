e7c2f549cb162a6f26c7c547ed1ba518d50f90da
kishore <217641713+KishoreKannaG2@users.noreply.github.com>
2026-05-04 00:01:57 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://secureauthlab.netlify.app/',
+  baseURL: 'https://secureauthlab.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
